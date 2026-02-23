da53c2837882f6672802fec8094b74cd4776b237
mansi2004-dotcom <mansi79730@gmail.com>
2026-02-23 20:21:12 +0530

    updated

---
+++ forloop_prac.py
@@ -0,0 +1,31 @@
+# #Count the number of vowels in a string.
+# string="Mansi"
+# vowels="aeiou"
+# count = 0
+# for i in string:
+#     if i in vowels:
+#         count += 1
+# print(count)
+# #Print all even numbers between 1 and 50.
+# for i in range(1,50):
+#     if i %2 == 0:
+#         print(i,end=",")
+# ##Find the sum of numbers from 1 to N.
+# n=int(input("enter number: "))
+# add = 0
+# for i in range(1,n+1):
+#     add += i
+# print(add)
+# #Count the number of vowels in a string.
+# v="Ridhima"
+# vowel = "aeiou"
+# count =0
+# for i in v:
+#     if i in vowel:
+#         count += 1
+# print(count)
+#Reverse a string using a for loop.
+str1="Mansi"
+for char in str1:
+    rev_str1=str1[-1]
+    print(rev_str1)
---
+++ list_practice.py
@@ -0,0 +1,33 @@
+# # find sum of list and count number of elements.
+# l=[30,40,50,60,80,23]
+# # total =0
+# count = 0
+# for i in l:
+#     # total += i
+#     count += 1
+# # print(total)
+# print(count)
+# # find largest number in list.
+# print(max(l))
+# # find minimum of list.
+# print(min(l))
+# # find count of even and odd number in list.
+# l1=[22,23,45,11,66,34,68,90,46,55]
+# even_count = 0
+# odd_count = 0
+# for i in l1:
+#     if i%2 == 0:
+#         even_count += 1
+#     else:
+#         odd_count += 1
+# print("Total even number in list:",even_count)
+# print("Total odd number in list:",odd_count)
+## Reverse a list.
+l1=[22,23,45,11,66,34,68,90,46,55]
+rev_l1= 0
+for i in l1:
+    rev_l1=l1[::-1]
+print(rev_l1)
+    
+l2=[22,45,32,56,44,22,56,22,77,34,22,89,22]
+print(l2.duplicate)
