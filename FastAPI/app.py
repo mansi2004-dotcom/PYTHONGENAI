e43ac13f652731fca8ce00cb29a1bf5c397fcfc3
mansi2004-dotcom <mansi79730@gmail.com>
2026-04-17 12:15:05 +0530

    push

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -1,7 +1,10 @@
 import uvicorn
-from fastapi import fastapi
+from fastapi import FastAPI
+from input import getData, heartInput
+import joblib
 
 app=FASTAPI()
+model = joblib.load('model.pkl')
 
 #get,post,update,delete - get is post data at backend but not secure,
 #backend p
@@ -13,9 +16,24 @@ def index():
 def index():
     return{'message':'hello Mansi'}
 # to run fastapi - uvicorn filename:objectname
-@app.get('/{name}')
-def getname(name:str):
-    return{'Name is':f'{name}'}
+@app.get('/name')
+def printnamm():
+    return{'message':'hello Mansi'}
+
+# #@app.get('/{name}')
+# def getName(name:str):
+#     return {"Name is":f'{name}'}
+
+# @app.post('/{name}')
+# def getName(name:str):
+#     return {"name is":f'{name}'}
+
+@app.post('/getname')
+def getName(data:getData):
+    dict_data=data.model_dump()
+    print(dict_data)
+
+    return dict_data
 
 
 if __name__ =='__main__':
--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, UploadFile, File, Depends
+from fastapi import FastAPI,UploadFile
 from pydantic import BaseModel
 from typing import List,Optional 
 
