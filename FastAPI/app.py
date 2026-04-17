17a4da6289f93ffc3dbf744b21bf706241a6a125
mansi2004-dotcom <mansi79730@gmail.com>
2026-04-17 12:45:52 +0530

    up

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -1,10 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
-from input import getData, heartInput
-import joblib
+from input import getData
+# import joblib
 
-app=FASTAPI()
-model = joblib.load('model.pkl')
+app=FastAPI()
+# model = joblib.load('model.pkl')
 
 #get,post,update,delete - get is post data at backend but not secure,
 #backend p
@@ -12,10 +12,10 @@ model = joblib.load('model.pkl')
 def index():
     return{'message':'hello students, how are you!'}
 
-@app.get('/name')
-def index():
-    return{'message':'hello Mansi'}
-# to run fastapi - uvicorn filename:objectname
+# @app.get('/name')
+# def index():
+#     return{'message':'hello Mansi'}
+# # to run fastapi - uvicorn filename:objectname
 @app.get('/name')
 def printnamm():
     return{'message':'hello Mansi'}
@@ -36,5 +36,37 @@ def getName(data:getData):
     return dict_data
 
 
-if __name__ =='__main__':
-    uvicorn.run(app,host='127.0.0.1', port =8000)
+# @app.post('/heartdisease')
+# def heartDis(data: heartInput):
+#     data= data.model_dump()
+#     age=data['age']
+#     sex=data['sex']
+#     cp= data['cp']
+#     trestbps= data['trestbps'] 
+#     chol= data['chol']
+#     fbs = data['fbs']
+#     restecg= data['restecg']
+#     thalach= data['thalach']
+#     exang= data['exang']
+#     oldpeak=data['oldpeak']
+#     slope= data['slope']
+#     ca= data['ca']
+#     thal= data['thal']
+    
+#     host_data= [age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]
+#     pred_result = model.predict([host_data])
+    
+#     if pred_result[0]==1:
+#         prediction= "Heart Disease Detected"
+#     else:
+#         prediction="No Heart Disease"
+        
+#     return prediction
+
+
+# to run fastapi - uvicorn filename:objectname --reload
+
+# if _name=='main_':
+#     uvicorn.run(app,host='127.0.0.1',port=8000)
+
+
---
+++ FastAPI/input.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class getData(BaseModel):
+    name:str
